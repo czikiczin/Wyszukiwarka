9e4b8e8d78e2b42c6a112918adcd8e4e20d3c45a
Grpat <73405378+Grpat@users.noreply.github.com>
2022-03-29 21:16:18 +0200

    Zmiany w routingu

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const viewController = require('../controllers/viewController.js');
+const express = require("express");
+const viewController = require("../controllers/viewController.js");
 
 const router = express.Router();
 
-router.get('/', viewController.getMainPage);
+//router.get("/", viewController.getMainPage);
 //router.get('/sc', scrapperController.scrapeData);
-router.get('/products', viewController.getAllProducts);
-router.get('/products/:id', viewController.getProductById);
+router.get("/", viewController.getAllProducts);
+router.get("/:id", viewController.getProductById);
+
 module.exports = router;
